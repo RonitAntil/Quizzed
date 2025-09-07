622d626928b5d33bd9838e5d48b0c257b321cc63
Ronit Antil <135525594+RonitAntil@users.noreply.github.com>
2025-09-07 08:20:17 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Allow frontend to connect to backend
+app.use(cors({
+  origin: [
+    "http://localhost:3000", // for local dev
+    "http://localhost:5000", // if testing backend locally
+    "https://quizzed-platform.netlify.app/" // Netlify domain
+  ],
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' })); // Parse JSON data from requests
 app.use(express.static('../frontend')); // Serve static files
 
